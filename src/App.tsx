8c91c9ab545413e1a8ab8b098ff266b17a823038
Aleksei Bykov <gogohandz@gmail.com>
2024-07-16 20:14:31 +0300

    Add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,63 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link
+} from "react-router-dom";
 import "./App.css"
 import { Counter } from "./features/counter/Counter"
+import ErrorPage from "./router/error-page"
+import { useState } from "react"
 import { Quotes } from "./features/quotes/Quotes"
-import logo from "./logo.svg"
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppTemp />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "Counter",
+      element: <Counter />,
+    },
+    {
+      path: "Quotes",
+      element: <Quotes />,
+    },
+  ]);
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+const AppTemp = () => {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Quotes />
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://reselect.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Reselect
-          </a>
-        </span>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Counter">Counter</Link>
+              </li>
+              <li>
+                <Link to="/Quotes">Quotes</Link>
+              </li>
+              <li>
+                <Link to="/nothing-here">Nothing Here</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     </div>
   )
